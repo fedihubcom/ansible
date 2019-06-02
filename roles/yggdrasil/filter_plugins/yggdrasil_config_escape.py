0c8e686bcd3fc0410349abdaa50ca81daa3bbea9
Alex Kotov <kotovalexarian@gmail.com>
2019-06-02 19:56:08 +0500

    Add filter "short_key"

---
+++ roles/yggdrasil/filter_plugins/yggdrasil_config_escape.py
@@ -0,0 +1,19 @@
+import re
+
+from ansible.errors import AnsibleFilterError
+
+SHORT_KEY_RE = re.compile(r'^[0-9a-z]{64}$', re.IGNORECASE)
+
+def short_key(s):
+    s = str(s)
+    if not SHORT_KEY_RE.match(s):
+        raise AnsibleFilterError('Invalid key: %s' % s)
+    return '"%s"' % s.lower()
+
+class FilterModule:
+    '''Filters to validate and escape Yggdrasil configuration variables.'''
+
+    def filters(self):
+        return {
+                'short_key': short_key,
+        }
