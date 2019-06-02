04dbd137b4f879642758ec59da9f176d1930f40a
Alex Kotov <kotovalexarian@gmail.com>
2019-06-02 19:59:59 +0500

    Add filter "long_key"

--- roles/yggdrasil/filter_plugins/yggdrasil_config_escape.py
+++ roles/yggdrasil/filter_plugins/yggdrasil_config_escape.py
@@ -3,10 +3,17 @@ import re
 from ansible.errors import AnsibleFilterError
 
 SHORT_KEY_RE = re.compile(r'^[0-9a-z]{64}$', re.IGNORECASE)
+LONG_KEY_RE  = re.compile(r'^[0-9a-z]{128}$', re.IGNORECASE)
 
 def short_key(s):
     s = str(s)
     if not SHORT_KEY_RE.match(s):
+        raise AnsibleFilterError('Invalid key: %s' % s)
+    return '"%s"' % s.lower()
+
+def long_key(s)
+    s = str(s)
+    if not LONG_KEY_RE.match(s):
         raise AnsibleFilterError('Invalid key: %s' % s)
     return '"%s"' % s.lower()
 
@@ -16,4 +23,5 @@ class FilterModule:
     def filters(self):
         return {
                 'short_key': short_key,
+                'long_key':  long_key,
         }
