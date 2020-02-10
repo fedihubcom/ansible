d3e7c04329fc7821140c7dcd4ef2dbe104cf5cc2
Alex Kotov <kotovalexarian@gmail.com>
2020-02-10 14:53:48 +0500

    Implement IP reporting

--- report_ip/config/application.rb
+++ report_ip/config/application.rb
@@ -7,8 +7,21 @@ Bundler.require
 
 module ReportIP
   class Application < Sinatra::Application
-    get '/' do
-      'Hello, World!'
+    post '/:domain/:secret' do
+      redis = Redis.new url: ENV['REDIS_URL']
+
+      domain = params[:domain].to_s.strip
+      got_secret = params[:secret].to_s.strip
+
+      expected_secret = redis.hget('secrets', domain).to_s.strip
+
+      if got_secret != expected_secret || expected_secret.empty?
+        halt 401, 'Unauthorized'
+      end
+
+      redis.hset 'ips', domain, request.ip
+
+      request.ip
     end
   end
 end
