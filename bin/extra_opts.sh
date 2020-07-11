aa90c5e689f17eeef6a851be69e6f01bd3db831e
Alex Kotov <kotovalexarian@gmail.com>
2020-07-11 14:32:37 +0500

    Improve experience for multiple admins

--- bin/extra_opts.sh
+++ bin/extra_opts.sh
@@ -1,12 +1,8 @@
-for vault_id in default kotovalexarian
+extra_opts="--extra-vars admin=$(cat admin)"
+
+for vault_id in default kotovalexarian xuhcc
 do
   if [ -f "secrets/$vault_id" ]; then
-    if [ -z "$extra_opts" ]; then
-      extra_opts="--vault-id"
-    else
-      extra_opts="$extra_opts --vault-id"
-    fi
-
-    extra_opts="$extra_opts $vault_id@secrets/$vault_id"
+    extra_opts="$extra_opts --vault-id $vault_id@secrets/$vault_id"
   fi
 done
