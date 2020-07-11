07a28c4089a167eb9ca9ad4f2696d3adc3cee4bf
Alex Kotov <kotovalexarian@gmail.com>
2020-07-11 14:49:39 +0500

    Improve scripts

--- bin/extra_opts.sh
+++ bin/extra_opts.sh
@@ -1,20 +1,24 @@
-if [ ! -f admin ]; then
+FILE=$(readlink -f "$0")
+DIR=$(dirname "$FILE")
+ROOT=$(dirname "$DIR")
+
+if [ ! -f "$ROOT/admin" ]; then
   >&2 echo 'error: admin is not specified'
   exit 1
 fi
 
-admin="$(cat admin)"
+admin="$(cat "$ROOT/admin")"
 
 if [ -z "$admin" ]; then
   >&2 echo 'error: admin is not specified'
   exit 1
 fi
 
-extra_opts="--extra-vars admin=$(cat admin)"
+extra_opts="--extra-vars admin=$admin"
 
 for vault_id in default kotovalexarian xuhcc
 do
-  if [ -f "secrets/$vault_id" ]; then
-    extra_opts="$extra_opts --vault-id $vault_id@secrets/$vault_id"
+  if [ -f "$ROOT/secrets/$vault_id" ]; then
+    extra_opts="$extra_opts --vault-id $vault_id@$ROOT/secrets/$vault_id"
   fi
 done
