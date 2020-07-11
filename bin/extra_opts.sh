c677d0d9e0e1e3e3a92953a744685a5910c3014c
Alex Kotov <kotovalexarian@gmail.com>
2020-07-11 14:48:03 +0500

    Verify configuration

--- bin/extra_opts.sh
+++ bin/extra_opts.sh
@@ -1,3 +1,15 @@
+if [ ! -f admin ]; then
+  >&2 echo 'error: admin is not specified'
+  exit 1
+fi
+
+admin="$(cat admin)"
+
+if [ -z "$admin" ]; then
+  >&2 echo 'error: admin is not specified'
+  exit 1
+fi
+
 extra_opts="--extra-vars admin=$(cat admin)"
 
 for vault_id in default kotovalexarian xuhcc
